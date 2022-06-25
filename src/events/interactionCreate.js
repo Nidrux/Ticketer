61343e89964bea8a80cb9b681a5b63e95fcb5771
Mezo <dominik@koch-bautechnik.de>
2022-06-25 18:51:09 +0200

    Dm bug fix
    The bot no longer crashes when using interactions in direct messages

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -4,33 +4,40 @@ const { errorLog } = require('../util/functions/error');
 
 /* Export */
 module.exports = (interaction) => {
-  const { client } = interaction;
-
-  if (!interaction.isCommand()) return;
-  const command = client.commands.get(interaction.commandName);
-  if (!command) return;
-  try {
-    Guild.findOne({ id: interaction.guild.id }).then((dbGuild) => {
-      if (!dbGuild) {
-        try {
-          const g = new Guild({
-            id: interaction.guild.id,
-            botJoined: (Date.now() / 1000) | 0,
-          });
-
-          g.save();
-          dbGuild = g;
-        } catch (e) {
-          errorLog('', client, e, interaction.guild.id, interaction.user.id, interaction.channel.id, `commands/${interaction.commandName}.js`);
-        }
-      }
-      command.execute(interaction, client, dbGuild);
-    });
-  } catch (err) {
-    if (err) console.error(err);
+  if (!interaction.guild) {
     interaction.reply({
-      content: 'An error occurred while executing that command.',
+      content: 'You need to be in a server to use this command.',
       ephemeral: true,
     });
+  } else {
+    const { client } = interaction;
+
+    if (!interaction.isCommand()) return;
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+    try {
+      Guild.findOne({ id: interaction.guild.id }).then((dbGuild) => {
+        if (!dbGuild) {
+          try {
+            const g = new Guild({
+              id: interaction.guild.id,
+              botJoined: (Date.now() / 1000) | 0,
+            });
+
+            g.save();
+            dbGuild = g;
+          } catch (e) {
+            errorLog('', client, e, interaction.guild.id, interaction.user.id, interaction.channel.id, `commands/${interaction.commandName}.js`);
+          }
+        }
+        command.execute(interaction, client, dbGuild);
+      });
+    } catch (err) {
+      if (err) console.error(err);
+      interaction.reply({
+        content: 'An error occurred while executing that command.',
+        ephemeral: true,
+      });
+    }
   }
 };
--- src/util/interactionHandler.js
+++ src/util/interactionHandler.js
@@ -9,33 +9,26 @@ const Guild = require('./models/guild');
 /* Export */
 module.exports = async (client) => {
   client.on('interactionCreate', async (interaction) => {
-    if (!interaction.guild) {
-      interaction.reply({
-        content: 'You need to be in a server to use this command.',
+    if (!interaction.customId) return;
+
+    const customId = interaction.customId.includes('_') ? interaction.customId.substring(0, interaction.customId.indexOf('_')) : interaction.customId;
+    try {
+      Guild.findOne({ id: interaction.guild.id }).then((dbGuild) => {
+        const interactions = readdirSync(join(__dirname, '../interactions')).filter((f) => f.endsWith('.js'));
+        if (!dbGuild || !interactions.includes(`${customId}.js`)) {
+          return interaction.reply({
+            content: 'An error occurred while executing that interaction.',
+            ephemeral: true,
+          });
+        }
+        require(`../interactions/${customId}`)(client, interaction, dbGuild);
+      });
+    } catch (e) {
+      errorLog('', client, e, interaction.guild.id, interaction.user.id, interaction.channel.id, `interactions/${interaction.commandName}.js`);
+      return interaction.reply({
+        content: 'An error occurred while executing that interaction.',
         ephemeral: true,
       });
-    } else {
-      if (!interaction.customId) return;
-
-      const customId = interaction.customId.includes('_') ? interaction.customId.substring(0, interaction.customId.indexOf('_')) : interaction.customId;
-      try {
-        Guild.findOne({ id: interaction.guild.id }).then((dbGuild) => {
-          const interactions = readdirSync(join(__dirname, '../interactions')).filter((f) => f.endsWith('.js'));
-          if (!dbGuild || !interactions.includes(`${customId}.js`)) {
-            return interaction.reply({
-              content: 'An error occurred while executing that interaction.',
-              ephemeral: true,
-            });
-          }
-          require(`../interactions/${customId}`)(client, interaction, dbGuild);
-        });
-      } catch (e) {
-        errorLog('', client, e, interaction.guild.id, interaction.user.id, interaction.channel.id, `interactions/${interaction.commandName}.js`);
-        return interaction.reply({
-          content: 'An error occurred while executing that interaction.',
-          ephemeral: true,
-        });
-      }
     }
   });
 };
